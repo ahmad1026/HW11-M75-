31985f4e0d2fa84a3029e5d713ee35b86e9c3c48
ahmad.mohammadi <a.mohammadi1026@gmail.com>
2022-06-04 16:58:15 +0430

    fixz bugs

--- q1-test/app.js
+++ q1-test/app.js
@@ -1,18 +1,37 @@
 let blockMenu = document.getElementById("block-menu");
 
-
+let navbtns = document.querySelectorAll('.nav-content a');
+Array.from(navbtns).forEach(function(btn){
+  btn.addEventListener('click',function(e){
+    e.target.style.backgroundColor = "#B4CD93";
+    navbtns.forEach(item=>{
+      if(e.target.id != item.id){
+        item.style.backgroundColor = "";
+      }
+    })
+  })
+});
 
 function closeMenu() {
-
-  blockMenu.classList.remove('d-block');
-  
+  blockMenu.classList.remove('menu-open');
+  blockMenu.classList.add('menu-close');
 }
-
-
 function openMenu(event) {
 
   event.stopPropagation();
-  blockMenu.classList.add('d-block');
+  blockMenu.classList.add('menu-open');
 
   
+}
+
+function changeTheme(event) {
+  if(event.target.value === 'on'){
+    blockMenu.classList.add('bg-dark');
+    event.target.value = 'off';
+  }else if(event.target.value === 'off'){
+    blockMenu.classList.remove('bg-dark');
+    blockMenu.classList.add('lightthem');
+    event.target.value = 'on';
+  }
+
 }
